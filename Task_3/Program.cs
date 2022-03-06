0ed98d487244b5667d70037c630b04d77b31c080
Katerina <ealmazurova@internet.ru>
2022-03-06 11:26:52 +0300

    Задача №3 на вывод таблицы квадратов введенного числа

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите N => ");
+string inputN = Console.ReadLine();
+int N = int.Parse(inputN);
+int i = 1;
+while (i <=N)
+{
+    int ans = i * i;
+    Console.Write(ans+" ");
+    i++;
+}
